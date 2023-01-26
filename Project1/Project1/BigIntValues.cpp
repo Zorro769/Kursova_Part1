9dc25a47cab2cd41a202fd40da7df78ab7928dac
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2023-01-26 21:56:16 +0200

    Revert "adding virtual"
    This reverts commit 50428d99427b0269ad510bb35e6d09d37f0dcf7f.

--- Project1/Project1/BigIntValues.cpp
+++ Project1/Project1/BigIntValues.cpp
@@ -2,24 +2,24 @@
 using std::cout;
 #pragma warning(disable : 4996)
 
-//BigIntValues::BigIntValues(char* s) {
-//	pStrStart = new char[strlen(s)];
-//	pStrStart[strlen(s)] = '\0';
-//	for (int i = strlen(s) - 1,j = 0; i >= 0; i--,j++)
-//	{
-//		if (!isdigit(s[i]))
-//			throw("ERROR");
-//		pStrStart[j] = s[i];
-//	}
-//	maxLength = NULL;
-//}
-//BigIntValues::BigIntValues(unsigned long long nr) {
-//	//pStrStart = new(std::nothrow) char[maxLength];
-//	do {
-//		pStrStart = Append(pStrStart,1,((nr % 10) + '0'));
-//		nr /= 10;
-//	} while (nr);
-//}
+BigIntValues::BigIntValues(char* s) {
+	pStrStart = new char[strlen(s)];
+	pStrStart[strlen(s)] = '\0';
+	for (int i = strlen(s) - 1,j = 0; i >= 0; i--,j++)
+	{
+		if (!isdigit(s[i]))
+			throw("ERROR");
+		pStrStart[j] = s[i];
+	}
+	maxLength = NULL;
+}
+BigIntValues::BigIntValues(unsigned long long nr) {
+	//pStrStart = new(std::nothrow) char[maxLength];
+	do {
+		pStrStart = Append(pStrStart,1,((nr % 10) + '0'));
+		nr /= 10;
+	} while (nr);
+}
 char* BigIntValues::Append(char* array,size_t n ,char a)
 {
 	size_t len = strlen(array);
@@ -74,188 +74,180 @@ char* BigIntValues::Resize(char* array,size_t size)
 	return ret;
 }
 
-
-//BigIntValues& BigIntValues::operator=(const BigIntValues& a)
-//{
-//	pStrStart = a.pStrStart;
-//	return *this;
-//}
-
-//CharacterString& BigIntValues::operator+(const CharacterString& a) {
-//	int t = 0, s = 0, i, m, n;
-//	CharacterString temp;
-//	temp.pStrStart = pStrStart;
-//	if (strlen(temp.pStrStart) == 0)
-//		n = strlen(temp.pStrStart) + 1;
-//	else
-//		n = strlen(temp.pStrStart);
-//	if (strlen(a.pStrStart) == 0)
-//		m = strlen(a.pStrStart) + 1;
-//	else
-//		m = strlen(a.pStrStart);
-//	if (m > n) {
-//		temp.pStrStart = BigIntValues::Append(temp.pStrStart, m - n, '0');
-//	}
-//	if (strlen(a.pStrStart) == 0)
-//		n = strlen(a.pStrStart) + 1;
-//	else
-//		n = strlen(a.pStrStart);
-//	for (i = 0; i < n; i++) {
-//		int temp1 = temp.pStrStart[i] - '0';
-//		int temp2 = a.pStrStart[i] - '0';
-//		if (i < m) {
-//			s = temp1 + temp2 + t;
-//		}
-//		else
-//			s = temp1 + t;
-//		t = s / 10;
-//		temp.pStrStart[i] = (s % 10) + '0';
-//	}
-//	if (t) {
-//		temp.pStrStart = BigIntValues::Append(temp.pStrStart, 1, t + '0');
-//	}
-//	//a.pStrStart = pStrStart;
-//	return temp;
-//}
-//CharacterString& BigIntValues::operator+(const CharacterString& a)
-//{
-//	CharacterString temp;
-//	temp.pStrStart = pStrStart;
-//	temp += a;
-//	return temp;
-//}
-
+BigIntValues& BigIntValues::operator=(const BigIntValues& a)
+{
+	pStrStart = a.pStrStart;
+	return *this;
+}
+BigIntValues& operator+=(BigIntValues& a, const BigIntValues& b) {
+	int t = 0, s=0, i,m,n;
+	if (strlen(a.pStrStart) == 0)
+		n = strlen(a.pStrStart) + 1;
+	else
+		n = strlen(a.pStrStart);
+	if (strlen(b.pStrStart) == 0)
+		m = strlen(b.pStrStart) + 1;
+	else
+		m = strlen(b.pStrStart);
+	if (m > n) {
+		a.pStrStart = a.Append(a.pStrStart, m - n, '0');
+	}
+	if(strlen(a.pStrStart) == 0)
+		n = strlen(a.pStrStart) + 1;
+	else
+		n = strlen(a.pStrStart);
+	for (i = 0; i < n; i++) {
+		int temp1 = a.pStrStart[i] - '0';
+		int temp2 = b.pStrStart[i] - '0';
+		if (i < m) {
+			s = temp1 + temp2 + t;
+		}
+		else
+			s = temp1 + t;
+		t = s / 10;
+		a.pStrStart[i] = (s % 10) + '0';
+	}
+	if (t) {
+		a.pStrStart = a.Append(a.pStrStart,1,t + '0');
+	}
+	return a;
+}
 //BigIntValues& operator+(const BigIntValues& a, const BigIntValues& b) {
 //	BigIntValues temp;
 //	temp = a;
 //	temp += b;
 //	return temp;
 //}
-
+BigIntValues& operator+(const BigIntValues& a){
+	BigIntValues temp;
+	temp = a;
+	temp += b;
+	return temp;
+}
 BigIntValues::~BigIntValues()
 {
 	//delete[] pStrStart;	
 }
-//bool operator<(const BigIntValues& a, const BigIntValues& b) {
-//	int n = strlen(a.pStrStart), m = strlen(b.pStrStart);
-//	if (n != m)
-//		return n < m;
-//	while (n--)
-//		if (a.pStrStart[n] != b.pStrStart[n])
-//			return a.pStrStart[n] < b.pStrStart[n];
-//	return false;
-//}
-//bool operator>(const BigIntValues& a, const BigIntValues& b)
-//{
-//	int n = strlen(a.pStrStart), m = strlen(b.pStrStart);
-//	if (n != m)
-//		return n > m;
-//	while (n--)
-//		if (a.pStrStart[n] != b.pStrStart[n])
-//			return a.pStrStart[n] > b.pStrStart[n];
-//	return false;
-//}
-//bool operator<=(const BigIntValues&, const BigIntValues&)
-//{
-//	return false;
-//}
-//bool operator>=(const BigIntValues&, const BigIntValues&)
-//{
-//	return false;
-//}
-//bool operator==(const BigIntValues& a, const BigIntValues& b) {
-//	if (strcmp(a.pStrStart, b.pStrStart) == 0)
-//		return true;
-//	return false;
-//}
-//BigIntValues& operator-=(BigIntValues& a, const BigIntValues& b) {
-//	if (a < b)
-//		throw("UNDERFLOW");
-//	int n, m;
-//	if (strlen(a.pStrStart) == 0)
-//		n = strlen(a.pStrStart) + 1;
-//	else
-//		n = strlen(a.pStrStart);
-//	if (strlen(b.pStrStart) == 0)
-//		m = strlen(b.pStrStart) + 1;
-//	else
-//		m = strlen(b.pStrStart);
-//	int i, t = 0, s;
-//	for (i = 0; i < n; i++) {
-//		int temp1 = a.pStrStart[i] - '0';
-//		int temp2 = b.pStrStart[i] - '0';
-//		if (i < m)
-//			s = temp1 - temp2 + t;
-//		else
-//			s = temp1 + t;
-//		if (s < 0)
-//			s += 10,
-//			t = -1;
-//		else
-//			t = 0;
-//		a.pStrStart[i] = s + '0';
-//	}
-//	while (n > 1 && a.pStrStart[n - 1] == '0') {
-//		a.pStrStart = a.PopBack(a.pStrStart);
-//		n--;
-//	}
-//	return a;
-//}
-//BigIntValues operator-(const BigIntValues& a, const BigIntValues& b) {
-//	BigIntValues temp;
-//	temp = a;
-//	temp -= b;
-//	return temp;
-//}
-//BigIntValues& operator*=(BigIntValues& a, const BigIntValues& b)
-//{
-//	if (Null(a) || Null(b)) {
-//		a = BigIntValues();
-//		return a;
-//	}
-//	int size = 0,n,m;
-//	if (strlen(a.pStrStart) == 0)
-//		n = strlen(a.pStrStart) + 1;
-//	else
-//		n = strlen(a.pStrStart);
-//	if (strlen(b.pStrStart) == 0)
-//		m = strlen(b.pStrStart) + 1;
-//	else
-//		m = strlen(b.pStrStart);
-//	//vector<int> v(n + m, 0);
-//	int* array = new int[n + m]{ 0 };
-//	for (int i = 0; i < n; i++) {
-//		int temp1 = a.pStrStart[i] - '0';
-//		for (int j = 0; j < m; j++) {
-//			int temp2 = b.pStrStart[j] - '0';
-//			array[i + j] += (temp1) * (temp2);
-//		}
-//	}
-//	size = n + m;
-//	//for (int i = 0; i < 200; i++)
-//	//{
-//	//	std::cout << array[i]<< std::endl;
-//	//}
-//	n += m;
-//	a.pStrStart = a.Resize(a.pStrStart, size);
-//	for (int s, i = 0, t = 0; i < n; i++)
-//	{
-//		s = t + array[i];
-//		array[i] = s % 10;	
-//		t = s / 10;
-//		a.pStrStart[i] = array[i] + '0';
-//	}
-//	a.pStrStart[n] = '\0';
-//	for (int i = n - 1; i >= 1 && !array[i]; i--)
-//		a.pStrStart = a.PopBack(a.pStrStart);
-//	return a;
-//}
-//BigIntValues operator*(const BigIntValues& a, const BigIntValues& b) {
-//	BigIntValues temp;
-//	temp = a;
-//	temp *= b;
-//	return temp;
-//}
+bool operator<(const BigIntValues& a, const BigIntValues& b) {
+	int n = strlen(a.pStrStart), m = strlen(b.pStrStart);
+	if (n != m)
+		return n < m;
+	while (n--)
+		if (a.pStrStart[n] != b.pStrStart[n])
+			return a.pStrStart[n] < b.pStrStart[n];
+	return false;
+}
+bool operator>(const BigIntValues& a, const BigIntValues& b)
+{
+	int n = strlen(a.pStrStart), m = strlen(b.pStrStart);
+	if (n != m)
+		return n > m;
+	while (n--)
+		if (a.pStrStart[n] != b.pStrStart[n])
+			return a.pStrStart[n] > b.pStrStart[n];
+	return false;
+}
+bool operator<=(const BigIntValues&, const BigIntValues&)
+{
+	return false;
+}
+bool operator>=(const BigIntValues&, const BigIntValues&)
+{
+	return false;
+}
+bool operator==(const BigIntValues& a, const BigIntValues& b) {
+	if (strcmp(a.pStrStart, b.pStrStart) == 0)
+		return true;
+	return false;
+}
+BigIntValues& operator-=(BigIntValues& a, const BigIntValues& b) {
+	if (a < b)
+		throw("UNDERFLOW");
+	int n, m;
+	if (strlen(a.pStrStart) == 0)
+		n = strlen(a.pStrStart) + 1;
+	else
+		n = strlen(a.pStrStart);
+	if (strlen(b.pStrStart) == 0)
+		m = strlen(b.pStrStart) + 1;
+	else
+		m = strlen(b.pStrStart);
+	int i, t = 0, s;
+	for (i = 0; i < n; i++) {
+		int temp1 = a.pStrStart[i] - '0';
+		int temp2 = b.pStrStart[i] - '0';
+		if (i < m)
+			s = temp1 - temp2 + t;
+		else
+			s = temp1 + t;
+		if (s < 0)
+			s += 10,
+			t = -1;
+		else
+			t = 0;
+		a.pStrStart[i] = s + '0';
+	}
+	while (n > 1 && a.pStrStart[n - 1] == '0') {
+		a.pStrStart = a.PopBack(a.pStrStart);
+		n--;
+	}
+	return a;
+}
+BigIntValues operator-(const BigIntValues& a, const BigIntValues& b) {
+	BigIntValues temp;
+	temp = a;
+	temp -= b;
+	return temp;
+}
+BigIntValues& operator*=(BigIntValues& a, const BigIntValues& b)
+{
+	if (Null(a) || Null(b)) {
+		a = BigIntValues();
+		return a;
+	}
+	int size = 0,n,m;
+	if (strlen(a.pStrStart) == 0)
+		n = strlen(a.pStrStart) + 1;
+	else
+		n = strlen(a.pStrStart);
+	if (strlen(b.pStrStart) == 0)
+		m = strlen(b.pStrStart) + 1;
+	else
+		m = strlen(b.pStrStart);
+	//vector<int> v(n + m, 0);
+	int* array = new int[n + m]{ 0 };
+	for (int i = 0; i < n; i++) {
+		int temp1 = a.pStrStart[i] - '0';
+		for (int j = 0; j < m; j++) {
+			int temp2 = b.pStrStart[j] - '0';
+			array[i + j] += (temp1) * (temp2);
+		}
+	}
+	size = n + m;
+	//for (int i = 0; i < 200; i++)
+	//{
+	//	std::cout << array[i]<< std::endl;
+	//}
+	n += m;
+	a.pStrStart = a.Resize(a.pStrStart, size);
+	for (int s, i = 0, t = 0; i < n; i++)
+	{
+		s = t + array[i];
+		array[i] = s % 10;	
+		t = s / 10;
+		a.pStrStart[i] = array[i] + '0';
+	}
+	a.pStrStart[n] = '\0';
+	for (int i = n - 1; i >= 1 && !array[i]; i--)
+		a.pStrStart = a.PopBack(a.pStrStart);
+	return a;
+}
+BigIntValues operator*(const BigIntValues& a, const BigIntValues& b) {
+	BigIntValues temp;
+	temp = a;
+	temp *= b;
+	return temp;
+}
 //BigIntValues& operator/=(BigIntValues& a, const BigIntValues& b)
 //{
 //	if (Null(b))
